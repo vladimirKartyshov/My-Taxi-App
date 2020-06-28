de8424814341184bd80d092b1478d35c7cd1ebee
vladimirKartyshov <vovan198080@inbox.ru>
2020-06-28 06:16:44 +0300

    Splash screen код

--- app/src/main/java/com/example/mytaxiapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/mytaxiapp/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.mytaxiapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,25 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    startActivity(new Intent(SplashScreenActivity.this,
+                            SignInActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
